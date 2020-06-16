3f64a9d445e64b72c5da977e6fb442751dc94a52
solobearus <ivansolobear@gmail.com>
2020-06-16 15:13:07 +0300

    Updates

---
+++ precache-manifest.5e3c5d72821ae681ceb397a3837f435e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "36f469ee057983e869ef",
+    "url": "/Snack-wars/static/css/main.464d1260.chunk.css"
+  },
+  {
+    "revision": "36f469ee057983e869ef",
+    "url": "/Snack-wars/static/js/main.f8c1d9a6.chunk.js"
+  },
+  {
+    "revision": "53acaa1c6de21201b23b",
+    "url": "/Snack-wars/static/js/runtime~main.bbb8100b.js"
+  },
+  {
+    "revision": "5a5a2a28f1176da7370c",
+    "url": "/Snack-wars/static/js/2.2595fafe.chunk.js"
+  },
+  {
+    "revision": "584abee1a701c1a78691b3635e41517f",
+    "url": "/Snack-wars/static/media/Fujimaru-Regular.584abee1.ttf"
+  },
+  {
+    "revision": "feb45f14a374652daccf0df5851b58d1",
+    "url": "/Snack-wars/static/media/bg2.feb45f14.jpg"
+  },
+  {
+    "revision": "4434792f9a471bc64ff3428a096962ff",
+    "url": "/Snack-wars/static/media/logo.4434792f.png"
+  },
+  {
+    "revision": "03b39b6b586c14140cc33d6487e59c45",
+    "url": "/Snack-wars/index.html"
+  }
+];
